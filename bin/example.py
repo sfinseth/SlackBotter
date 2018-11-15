da79fd40e100fa1e140d6a8c601ff7208429cbd1
Stefan Finseth <stefanfinseth@gmail.com>
2018-11-15 13:09:09 +0100

    Version 1.2
    * Change recurring function timing to be controlled by the bot by
    passing an interval in seconds rather than controlling it inside the
    usage of the bot.
    * Add functionality for simple flows that presents a series of multiple
    choice options -> This is very basic and doesn't constitute a full
    nested tree structure, but rather a small set of related choices (not
    useful for the intended purpose of release flows as demonstrated in the
    example)

--- bin/example.py
+++ bin/example.py
@@ -2,7 +2,6 @@ import time
 from slackbotter.bot import SlackBotter
 from slackbotter.dictstore import DictStore
 
-
 slack_channel = 'test_channel'  # Replace with channel to write in
 slack_token = 'xoxb-2913143739-474123084806-ueBq5cM06ocnJ4Nhs9FNc3Pd'  # Replace with slackbot token
 bot = SlackBotter(slack_token, slack_channel)
@@ -20,9 +19,36 @@ def hello_world(args):  # { message }
     store.write(sent_messages)
 
 
+# This is a really bad example of what flows in their current form are capable of
+# The current flow capabilities are too simpel to support deployment flows -> this would need a proper
+# decision tree structure instead of the simple multi-choice steps outlined in the current version.
+# A later version will allow a better way to control nested options in a flow.
+def deploy_service(args):  # { service, environment, version, confirm }
+    """
+    :type args: dict
+    """
+    bot.send_message('Starting deployment:\n'
+                     'Service: *{}*\n'
+                     'Version: *{}*\n'
+                     'Environment: *{}*\n'
+                     'Please wait...'.format(args['service'], args['version'], args['environment']))
+    time.sleep(3)
+    bot.send_message('Finished deployment, running tests..\n'
+                     'Please wait...')
+    time.sleep(4)
+    bot.send_message('Tests passed, deploy successful')
+
+
 bot.add_trigger('hello', hello_world)
 bot.add_parameter('hello', '-m', 'message', ['test', 'world', 'horse'])
 bot.add_help_message('hello', 'Does something with the message input')
+
+bot.flow_create('deploy', 'Starting deploy flow!')
+bot.flow_step('deploy', 'service', 'Which service would you like to deploy? ', ['ott', 'oase-epg'])
+bot.flow_step('deploy', 'environment', 'Which environment would you like to deploy to? ', ['test', 'staging'])
+bot.flow_step('deploy', 'version', 'Which version would you like to deploy? ', ['1.0.0', '1.0.1', '1.1.0', '1.2.0'])
+bot.flow_step('deploy', 'confirm', 'please approve/deny', ['approve', 'deny'])
+bot.flow_action('deploy', deploy_service)
 
 while True:
     bot.run_connection()
--- slackbotter/bot.py
+++ slackbotter/bot.py
@@ -1,3 +1,4 @@
+import time
 from slackclient import SlackClient
 
 
@@ -11,9 +12,10 @@ class SlackBotter(object):
         self.help_messages = {
             'unknown': 'Use !help to get help; available commands are:\n',
         }
-        self.recurring_functions = []
+        self.recurring_functions = {}
         self.parameters = {}
         self.allowed_values = {}
+        self.flows = {}
         self.slack_client = SlackClient(token)
         return
 
@@ -25,21 +27,43 @@ class SlackBotter(object):
         if ('!{}'.format(trigger)) not in self.parameters.keys():
             self.parameters['!{}'.format(trigger)] = {}
         self.parameters['!{}'.format(trigger)][flag] = name
-        if('!{}'.format(trigger)) not in self.allowed_values.keys():
+        if ('!{}'.format(trigger)) not in self.allowed_values.keys():
             self.allowed_values['!{}'.format(trigger)] = {}
         if allowed_values:
             self.allowed_values['!{}'.format(trigger)][flag] = allowed_values
         return
 
-    def add_recurring_function(self, command):
-        self.recurring_functions.append(command)
+    def flow_create(self, keyword: str, message: str):
+        if '!{}'.format(keyword) not in self.flows.keys():
+            self.flows['!{}'.format(keyword)] = {
+                'message': message,
+                'steps': {},
+                'action': None
+            }
+
+    def flow_step(self, keyword: str, step: str, message: str, allowed_values: list = None):
+        if step not in self.flows['!{}'.format(keyword)]['steps'].keys():
+            self.flows['!{}'.format(keyword)]['steps'][step] = {
+                'message': message,
+                'options': allowed_values
+            }
+
+    def flow_action(self, keyword, command):
+        self.flows['!{}'.format(keyword)]['action'] = command
+        print(self.flows)
+
+    def add_recurring_function(self, command, interval):
+        self.recurring_functions[command] = {
+            'lastrun': time.time(),
+            'interval': interval
+        }
         return
 
     def add_help_message(self, trigger: str, message: str):
         self.help_messages[trigger] = message
         return
 
-    def check_message(self, message: str):
+    def check_message(self, message: str, sender: str):
         command = message.split(' ', 1)[0]
         try:
             if command in self.triggers:
@@ -54,7 +78,9 @@ class SlackBotter(object):
                         return
                 else:
                     self.triggers[command]()
-        except IndexError as e:
+            if command in self.flows.keys():
+                self.handle_flow(command, sender)
+        except IndexError:
             self.triggers['!help']('unknown')
         return
 
@@ -75,6 +101,33 @@ class SlackBotter(object):
                 self.send_help_message(command[1:])
         return params
 
+    def handle_flow(self, command: str, sender: str):
+        self.send_message(self.flows[command]['message'])
+        args = {}
+        for step in self.flows[command]['steps']:
+            self.send_message(self.flows[command]['steps'][step]['message'])
+            args[step] = None
+            while not args[step]:
+                incoming = self.slack_client.rtm_read()
+                for inc in incoming:
+                    if 'type' in inc and 'user' in inc:
+                        if inc['user'] != sender:
+                            self.send_message('Cannot handle new requests during a flow...')
+                            continue
+                        if inc['type'] == 'message':
+                            if 'text' in inc:
+                                if inc['text'] in self.flows[command]['steps'][step]['options']:
+                                    args[step] = inc['text']
+                                elif inc['text'] == 'help':
+                                    self.send_message('Allowed values:\n*- {}*'.format(
+                                        '*\n*- '.join(self.flows[command]['steps'][step]['options'])))
+                                elif inc['text'] == 'abort':
+                                    self.send_message('Aborting flow')
+                                    return
+                                else:
+                                    self.send_message('Invalid option')
+        self.flows[command]['action'](args)
+
     def send_message(self, msg: str):
         self.slack_client.rtm_send_message(self.channel, msg)
         return
@@ -86,7 +139,9 @@ class SlackBotter(object):
             self.send_message(self.help_messages['unknown'])
 
     def run_connection(self):
-        for t in sorted(self.triggers):
+        triggers = list(self.triggers.keys())
+        triggers.extend(list(self.flows.keys()))
+        for t in sorted(triggers):
             self.help_messages['unknown'] += '`{}`\n'.format(t.replace('!', ''))
         if self.slack_client.rtm_connect():
             while True:
@@ -95,9 +150,13 @@ class SlackBotter(object):
                     if 'type' in inc and 'user' in inc:
                         if inc['type'] == 'message':
                             if 'text' in inc:
-                                self.check_message(inc['text'])
+                                self.check_message(inc['text'], inc['user'])
                 for command in self.recurring_functions:
-                    command()
+                    if time.time() > self.recurring_functions[command]['lastrun'] + \
+                            self.recurring_functions[command]['interval']:
+                        self.recurring_functions[command]['lastrun'] = time.time()
+                        command()
+
         else:
             print('Connection failed, invalid token?')
         return
