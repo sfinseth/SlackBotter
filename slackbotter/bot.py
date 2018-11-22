6f9d1665f4569def3de129801903562eae467842
Stefan Finseth <stefanfinseth@gmail.com>
2018-11-22 16:12:19 +0100

    Improvements to how flows are handled
    * Flows can now be given a set of allowed values or an allowed regex
    pattern
    * Flows are no longer using the old command structure with ! - They are
    isntead invoked by using @[bot-name] [keyword]
    * General improvements to flows
    * Help messages for flows have been vastly improved to differentiate
    them from commands
    * Will now almost always reply in threads rather than posting to
    main channel to keep things more tidy
    * Less data loaded on startup -> now requires channel id rather than
    name to connect properly

--- slackbotter/bot.py
+++ slackbotter/bot.py
@@ -1,9 +1,10 @@
+import re
 import time
 from slackclient import SlackClient
 
 
 class SlackBotter(object):
-    def __init__(self, token: str, channel: str):
+    def __init__(self, token: str, channel: str, rtm_read_delay: int):
         self.token = token
         self.channel = channel
         self.triggers = {
@@ -17,6 +18,9 @@ class SlackBotter(object):
         self.allowed_values = {}
         self.flows = {}
         self.slack_client = SlackClient(token)
+        self.rtm_read_delay = rtm_read_delay
+        self.bot_id = None
+        self.MENTION_REGEX = "^<@(|[WU].+?)>(.*)"
         return
 
     def add_trigger(self, keyword: str, command):
@@ -34,23 +38,24 @@ class SlackBotter(object):
         return
 
     def flow_create(self, keyword: str, message: str):
-        if '!{}'.format(keyword) not in self.flows.keys():
-            self.flows['!{}'.format(keyword)] = {
+        if keyword not in self.flows.keys():
+            self.flows[keyword] = {
                 'message': message,
                 'steps': {},
                 'action': None
             }
 
-    def flow_step(self, keyword: str, step: str, message: str, allowed_values: list = None):
-        if step not in self.flows['!{}'.format(keyword)]['steps'].keys():
-            self.flows['!{}'.format(keyword)]['steps'][step] = {
+    def flow_step(self, keyword: str, step: str, message: str, allowed_values: list = None,
+                  allowed_pattern: str = None):
+        if step not in self.flows['{}'.format(keyword)]['steps'].keys():
+            self.flows[keyword]['steps'][step] = {
                 'message': message,
-                'options': allowed_values
+                'values': allowed_values,
+                'pattern': allowed_pattern
             }
 
     def flow_action(self, keyword, command):
-        self.flows['!{}'.format(keyword)]['action'] = command
-        print(self.flows)
+        self.flows[keyword]['action'] = command
 
     def add_recurring_function(self, command, interval):
         self.recurring_functions[command] = {
@@ -63,7 +68,7 @@ class SlackBotter(object):
         self.help_messages[trigger] = message
         return
 
-    def check_message(self, message: str, sender: str):
+    def check_message(self, message: str, sender: str, thread_ts: str):
         command = message.split(' ', 1)[0]
         try:
             if command in self.triggers:
@@ -71,21 +76,24 @@ class SlackBotter(object):
                     self.send_help_message(message.split(' ')[1])
                     return None
                 if command in self.parameters.keys():
-                    args = self.parse_args(command, message.split(' ', 1)[1])
+                    args = self.parse_args(command, message.split(' ', 1)[1], thread_ts)
                     if args:
                         self.triggers[command](args)
                     else:
                         return
                 else:
                     self.triggers[command]()
-            if command in self.flows.keys():
-                self.handle_flow(command, sender)
+            user_id, msg = self.parse_direct_mention(message)
+            if user_id == self.bot_id:
+                command = msg.split(' ', 1)[0]
+                if command in self.flows.keys():
+                    self.handle_flow(command, sender, thread_ts)
         except IndexError:
             self.triggers['!help']('unknown')
         return
 
-    def parse_args(self, command: str, args: str):
-        params = {}
+    def parse_args(self, command: str, args: str, thread_ts: str):
+        params = {'thread_ts': thread_ts}
         for k, v in self.parameters[command].items():
             try:
                 param_value = args.split(str('{} '.format(k)), 1)[1].lstrip().split(' ')[0]
@@ -94,42 +102,78 @@ class SlackBotter(object):
                     if param_value not in self.allowed_values[command][k]:
                         self.send_message('Invalid Paramater value: *{}*\n for parameter *{} ({})*'
                                           ', should be one of:\n *- {}*'
-                                          .format(param_value, v, k, '*\n*- '.join(self.allowed_values[command][k])))
+                                          .format(param_value, v, k, '*\n*- '.join(self.allowed_values[command][k])),
+                                          thread=thread_ts)
                         return
             except IndexError:
-                self.send_message('Parameter *{}* is missing.'.format(v))
+                self.send_message('Parameter *{}* is missing.'.format(v), thread=thread_ts)
                 self.send_help_message(command[1:])
         return params
 
-    def handle_flow(self, command: str, sender: str):
-        self.send_message(self.flows[command]['message'])
-        args = {}
+    def parse_direct_mention(self, message: str):
+        matches = re.search(self.MENTION_REGEX, message)
+        return (matches.group(1), matches.group(2).strip()) if matches else (None, None)
+
+    def handle_flow(self, command: str, sender: str, thread_ts: str):
+        self.send_message(self.flows[command]['message'], thread_ts)
+        args = {'thread_ts': thread_ts}
         for step in self.flows[command]['steps']:
-            self.send_message(self.flows[command]['steps'][step]['message'])
+            self.send_message(self.flows[command]['steps'][step]['message'], thread_ts)
             args[step] = None
             while not args[step]:
                 incoming = self.slack_client.rtm_read()
                 for inc in incoming:
                     if 'type' in inc and 'user' in inc:
-                        if inc['user'] != sender:
-                            self.send_message('Cannot handle new requests during a flow...')
+                        if inc['user'] != sender or inc['thread_ts'] != thread_ts:
+                            self.check_message(inc['text'], inc['user'], inc['thread_ts'])
                             continue
                         if inc['type'] == 'message':
                             if 'text' in inc:
-                                if inc['text'] in self.flows[command]['steps'][step]['options']:
-                                    args[step] = inc['text']
+                                if self.flows[command]['steps'][step]['values']:
+                                    if inc['text'] in self.flows[command]['steps'][step]['values']:
+                                        args[step] = inc['text']
+                                        continue
+                                    elif inc['text'] == 'help':
+                                        if self.flows[command]['steps'][step]['values']:
+                                            self.send_message('Allowed values:\n*- {}*'.format(
+                                                '*\n*- '.join(self.flows[command]['steps'][step]['values'])), thread_ts)
+                                            continue
+                                    elif inc['text'] == 'abort':
+                                        self.send_message('Aborting flow', thread_ts)
+                                        return
+                                    else:
+                                        self.send_message('Invalid option', thread_ts)
+                                elif self.flows[command]['steps'][step]['pattern']:
+                                    if inc['text'] == 'help':
+                                        if self.flows[command]['steps'][step]['pattern']:
+                                            self.send_message('Allowed pattern:\n*- {}*'.format(
+                                                self.flows[command]['steps'][step]['pattern']), thread_ts)
+                                            continue
+                                    elif inc['text'] == 'abort':
+                                        self.send_message('Aborting flow', thread_ts)
+                                        return
+                                    matches = re.search(self.flows[command]['steps'][step]['pattern'], inc['text'])
+                                    if matches is not None:
+                                        args[step] = inc['text']
+                                        continue
+                                    else:
+                                        self.send_message('Invalid option, must match pattern: {}'.format(
+                                            self.flows[command]['steps'][step]['pattern']), thread_ts)
+                                        continue
+                                elif inc['text'] == 'abort':
+                                    self.send_message('Aborting flow', thread_ts)
+                                    return
                                 elif inc['text'] == 'help':
                                     self.send_message('Allowed values:\n*- {}*'.format(
-                                        '*\n*- '.join(self.flows[command]['steps'][step]['options'])))
-                                elif inc['text'] == 'abort':
-                                    self.send_message('Aborting flow')
-                                    return
+                                        '*\n*- '.join(self.flows[command]['steps'][step]['options'])), thread_ts)
+                                    continue
                                 else:
-                                    self.send_message('Invalid option')
+                                    args[step] = inc['text']
+                                    continue
         self.flows[command]['action'](args)
 
-    def send_message(self, msg: str):
-        self.slack_client.rtm_send_message(self.channel, msg)
+    def send_message(self, msg: str, thread: str = None):
+        self.slack_client.rtm_send_message(self.channel, msg, thread)
         return
 
     def send_help_message(self, trigger: str):
@@ -139,23 +183,28 @@ class SlackBotter(object):
             self.send_message(self.help_messages['unknown'])
 
     def run_connection(self):
-        triggers = list(self.triggers.keys())
-        triggers.extend(list(self.flows.keys()))
-        for t in sorted(triggers):
+        for t in sorted(self.triggers.keys()):
             self.help_messages['unknown'] += '`{}`\n'.format(t.replace('!', ''))
-        if self.slack_client.rtm_connect():
+        if self.flows.keys():
+            self.help_messages['unknown'] += '\nAvailable mentions are:\n'
+            for t in sorted(self.flows.keys()):
+                self.help_messages['unknown'] += '`{}`\n'.format(t)
+
+        if self.slack_client.rtm_connect(with_team_state=False):
+            self.bot_id = self.slack_client.api_call('auth.test')['user_id']
             while True:
                 incoming = self.slack_client.rtm_read()
                 for inc in incoming:
                     if 'type' in inc and 'user' in inc:
                         if inc['type'] == 'message':
                             if 'text' in inc:
-                                self.check_message(inc['text'], inc['user'])
+                                self.check_message(inc['text'], inc['user'], inc['ts'])
                 for command in self.recurring_functions:
                     if time.time() > self.recurring_functions[command]['lastrun'] + \
                             self.recurring_functions[command]['interval']:
                         self.recurring_functions[command]['lastrun'] = time.time()
                         command()
+                time.sleep(self.rtm_read_delay)
 
         else:
             print('Connection failed, invalid token?')
