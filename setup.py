3dffe6980c741f485cb1d7f9a8854f69a184cf53
Stefan Finseth <stefanfinseth@gmail.com>
2018-11-23 09:30:22 +0100

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='SlackBotter',
-    version='1.2',
+    version='1.2.1',
     author='Stefan Finseth',
     author_email='stefanfinseth@gmail.com',
     packages=['slackbotter' ],
